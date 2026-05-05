83fad1096e6a564525bbec8188f86e840788636c
shabukumaresan <shabuk2002@gmail.com>
2026-05-05 14:54:22 +0530

    vite

--- tracking_customer/vite.config.js
+++ tracking_customer/vite.config.js
@@ -8,14 +8,15 @@
 //   ],
 // })
 
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["softwarestack.xyz"],
+    host: true, // allow external access
+    allowedHosts: [
+      ".softwarestack.xyz", // ✅ allows ALL subdomains
+    ],
   },
 });
